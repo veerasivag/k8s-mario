59ddf22b394644f8276e6b0dbe01c00f923520f9
malikireddykarthikreddy <malikireddykr@gmail.com>
2024-09-25 22:38:37 +0530

    Update backend.tf

--- EKS-TF/backend.tf
+++ EKS-TF/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
-    bucket = "projectmariobucket" # Replace with your actual S3 bucket name
+    bucket = "projectmario" # Replace with your actual S3 bucket name
     key    = "EKS/terraform.tfstate"
-    region = "ap-south-1"
+    region = "us-east-1"
   }
 }

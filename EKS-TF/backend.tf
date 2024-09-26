8806c99f45950603c4f50dcfe9bf53b17c66f946
veerasivag <sivathumpella@gmail.com>
2024-09-26 12:33:59 +0530

    Update backend.tf

--- EKS-TF/backend.tf
+++ EKS-TF/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
-    bucket = "projectmario11" # Replace with your actual S3 bucket name
+    bucket = "projectmario1ohio" # Replace with your actual S3 bucket name
     key    = "EKS/terraform.tfstate"
-    region = "ap-southeast-1"
+    region = "us-east-2"
   }
 }
